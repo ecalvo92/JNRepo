77e4ce37f3a251409a1921d226c0f11fcbeccee6
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-09-17 20:48:34 -0600

    Final de Semana 2

--- JN_WEB/JN_WEB/Controllers/HomeController.cs
+++ JN_WEB/JN_WEB/Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using JN_WEB.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -9,17 +8,6 @@ namespace JN_WEB.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
